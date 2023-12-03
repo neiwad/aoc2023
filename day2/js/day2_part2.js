84838ec9780c4e6ddd497ecf0a7957919aedf50e
NEIWAD (Damien Lch) <hello@neiwad.dev>
2023-12-03 19:37:01 +0100

    DAY2: Part2 Completed ✅

--- day2/js/day2_part2.js
+++ day2/js/day2_part2.js
@@ -1,8 +1,6 @@
 import fs from 'fs';
 import readline from 'readline';
 
-const cubes = { red: 12, green: 13, blue: 14 }
-
 export default function day2_part2() {
     var input_file = '../input.txt';
     var r = readline.createInterface({
@@ -18,22 +16,26 @@ export default function day2_part2() {
 
         const gameSets = text.split(':')[1].split(';')
 
-        let isGameValid = true
+        let gameSetMinColors = {
+            red: 0, green: 0, blue: 0
+        }
         gameSets.forEach(gameSet => {
+
             const colorValues = gameSet.split(',')
             colorValues.forEach(colorValue => {
-                const color = colorValue.split(' ').slice(-1)
+                const color = colorValue.split(' ').slice(-1)[0]
                 const value = colorValue.split(' ')[1]
 
-                if (cubes[color] < value) {
-                    isGameValid = false
+                if (gameSetMinColors[color] < +value) {
+                    gameSetMinColors[color] = +value
                 }
             })
+
+
+
         })
 
-        if (isGameValid) {
-            total += parseInt(gameNumber)
-        }
+        total += gameSetMinColors.red * gameSetMinColors.green * gameSetMinColors.blue
 
     }).on('close', () => {
         console.log("Part2: ", total);
