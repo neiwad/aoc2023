53ebb22b436608121ff17ae275153e5da4bb4bdb
NEIWAD (Damien Lch) <hello@neiwad.dev>
2023-12-03 19:18:15 +0100

    DAY2: Part1 Completed ✅

---
+++ day2/js/day2_part2.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const cubes = { red: 12, green: 13, blue: 14 }
+
+export default function day2_part2() {
+    var input_file = '../input.txt';
+    var r = readline.createInterface({
+        input: fs.createReadStream(input_file)
+    });
+
+    let total = 0
+
+    r.on('line', function (text) {
+
+        // Game number
+        const gameNumber = text.split(':')[0].split(' ')[1]
+
+        const gameSets = text.split(':')[1].split(';')
+
+        let isGameValid = true
+        gameSets.forEach(gameSet => {
+            const colorValues = gameSet.split(',')
+            colorValues.forEach(colorValue => {
+                const color = colorValue.split(' ').slice(-1)
+                const value = colorValue.split(' ')[1]
+
+                if (cubes[color] < value) {
+                    isGameValid = false
+                }
+            })
+        })
+
+        if (isGameValid) {
+            total += parseInt(gameNumber)
+        }
+
+    }).on('close', () => {
+        console.log("Part2: ", total);
+    })
+}
---
+++ day2/js/index.js
@@ -0,0 +1,5 @@
+import day2_part1 from './day2_part1.js';
+import day2_part2 from './day2_part2.js';
+
+day2_part1();
+day2_part2()
